21fc7e2c5370559d38bec2bd6f08d62209e20023
wmastersPlym <43572947+wmastersPlym@users.noreply.github.com>
2020-01-13 22:46:01 +0000

    Added tests for hash.java

---
+++ PatientManagementSystem/src/test/java/Model/Util/HashTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.Util;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author wmasters
+ */
+public class HashTest {
+    
+    public HashTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testHashPassword() {
+        assertTrue(!( Hash.hashPassword("test").equals("test") ) && (Hash.hashPassword("test") != null));
+    }
+    
+}
